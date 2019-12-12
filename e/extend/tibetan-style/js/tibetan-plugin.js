917ff9134f545bc31c90bb6f9562e9ab924e4fcc
Bumpa Gyal <32565374+bumpagyal@users.noreply.github.com>
2019-12-12 09:48:48 +0800

    Update tibetan-plugin.js

--- e/extend/tibetan-style/js/tibetan-plugin.js
+++ e/extend/tibetan-style/js/tibetan-plugin.js
@@ -4,7 +4,7 @@ if(ua.match(win)!== null){
 	var link = document.createElement("link");
 	link.rel = "stylesheet";
 	link.type = "text/css";
-	link.href = "/e/extend/tibetan-style/css/front-end-win.css";
+	link.href = "../../../../e/extend/tibetan-style/css/front-end-win.css";
 	link.media = "all";
 	var head = document.getElementsByTagName("head")[0];
 	head.appendChild(link);
